806338eb724f8498e033491d22abd950d7e25b68
Ishita <ishitakundi4444@gmail.com>
2023-01-07 16:33:23 +0530

    some cgane in price filter

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -78,12 +78,12 @@ exports.forgotPassword = catchAsyncErrors(async (req, res, next) => {
 
   await user.save({ validateBeforeSave: false });
 
-  const resetPasswordUrl = `${req.protocol}://${req.get(
-    "host"
-  )}/password/reset/${resetToken}`;
+  // const resetPasswordUrl = `${req.protocol}://${req.get(
+  //   "host"
+  // )}/password/reset/${resetToken}`;
 
   // temporary token url - for testing
-  // const resetPasswordUrl = `${process.env.FRONTEND_URL}/password/reset/${resetToken}`;
+  const resetPasswordUrl = `${process.env.FRONTEND_URL}/password/reset/${resetToken}`;
 
   const message = `Click on the link to reset password :- \n\n ${resetPasswordUrl} \n\n If you have not requested this email then please ignore it.`;
 
